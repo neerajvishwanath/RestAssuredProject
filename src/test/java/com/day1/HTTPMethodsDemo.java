1a24c4239ba52cac1097f213da8c6ff2ed3d1ca5
2108263 <2108263@LTIN504731.cts.com>
2026-02-09 07:58:39 +0530

    Added HTTP PUT Method

--- src/test/java/com/day1/HTTPMethodsDemo.java
+++ src/test/java/com/day1/HTTPMethodsDemo.java
@@ -6,7 +6,10 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class HTTPMethodsDemo {
-	@Test(enabled = false)
+	
+	String deviceId;
+	
+	@Test(priority = 1, enabled = false)
 	void getDevice() {
 		given()
 		.when()
@@ -18,9 +21,10 @@ public class HTTPMethodsDemo {
 			.log().all();
 	}
 	
-	@Test
+	@Test(priority = 2)
 	void postDevice() {
-		given()
+		
+		deviceId = given()
 			.contentType("application/json")
 			.body("{\r\n"
 					+ "   \"name\": \"HP Probook 04\",\r\n"
@@ -33,6 +37,32 @@ public class HTTPMethodsDemo {
 					+ "}")
 		.when()
 			.post("https://api.restful-api.dev/objects")
+		.then()
+			.statusCode(200)
+			.header("Content-Type",equalTo("application/json"))
+			.time(lessThan(2000L))
+			.body("name", equalTo("HP Probook 04"))
+			.body(containsString("id"))
+			.log().all()
+			.extract().jsonPath().getString("id");
+			
+	}
+	
+	@Test(priority = 3, dependsOnMethods = {"postDevice"})
+	void putDevice() {
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "   \"name\": \"HP Probook 04\",\r\n"
+					+ "   \"data\": {\r\n"
+					+ "      \"year\": 2020,\r\n"
+					+ "      \"price\": 1000.99,\r\n"
+					+ "      \"CPU model\": \"Intel Core i7\",\r\n"
+					+ "      \"Hard disk size\": \"500 GB\"\r\n"
+					+ "   }\r\n"
+					+ "}")
+		.when()
+			.put("https://api.restful-api.dev/objects/"+deviceId)
 		.then()
 			.statusCode(200)
 			.header("Content-Type",equalTo("application/json"))
